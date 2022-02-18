a3b0aaa1d3f9bf1d87539c31ba44639dfd57fb62
mni-dev <nazrulislam01531@gmail.com>
2022-02-18 22:51:17 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,49 @@
+//variable
+var deposit = 400;
+//condition
+if(deposit> 500){
+
+}
+if(deposit < 700){
+
+}
+if(deposit == 900){
+
+}
+if(deposit>=500){
+
+}
+if(deposit<=600){
+
+}
+if(deposit!=800){
+
+}
+else{
+
+}
+//Array
+const numbers = [45, 49, 80, 90, 39];
+const numberCount = numbers.length;
+numbers.push(79);
+numbers.pop();
+numbers[3] = 666;
+//Check if in the Array list
+if(numbers.indexOf(888 != -1)){
+
+}
+if(numbers.includes(6890)){
+
+}
+//Loop
+//while loop, for loop
+for(const num of numbers){
+
+}
+//function
+function fullName (first, second){
+    const name = first + ' ' + second;
+}
+const person= fullName('Abdur', 'Rahman');
+//Object
+const watch = {color: 'black', pric: 5900, brand: 'quartz'}
